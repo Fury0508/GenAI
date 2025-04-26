e92e54bf4a54d5c8bae3d698c5b963e59cde8730
Fury0508 <daan0508@gmail.com>
2025-04-26 20:04:06 +0100

    Worked on a project where added a extarcted the data from URL

---
+++ rag/rag_basic/rag_1.py
@@ -0,0 +1,69 @@
+from langchain_community.document_loaders import PyPDFLoader
+from pathlib import Path
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_qdrant import QdrantVectorStore
+from dotenv import load_dotenv
+from openai import OpenAI
+import json
+import os
+load_dotenv()
+client = OpenAI()
+pdf_path = Path(__file__).parent / "FinalDraft.pdf"
+
+loader = PyPDFLoader(file_path=pdf_path)
+docs = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 1000,
+    chunk_overlap = 200
+)
+
+split_docs = text_splitter.split_documents(documents=docs)
+embedder = OpenAIEmbeddings(
+    model = "text-embedding-3-large",
+    api_key= os.environ['OPENAI_API_KEY']
+)
+
+vector_store = QdrantVectorStore.from_documents(
+    documents=[],
+    url = "http://localhost:6333",
+    collection_name="learning_langchain",
+    embedding=embedder
+)
+
+vector_store.add_documents(documents = split_docs)
+print("Ingestion done")
+
+# print("DOCS",len(docs))
+# print("SPLIT",len(split_docs))
+
+retriver = QdrantVectorStore.from_existing_collection(
+    url = "http://localhost:6333",
+    collection_name="learning_langchain",
+    embedding=embedder
+)
+
+relevant_chunks  = retriver.similarity_search(
+    query="What is BERT?"
+)
+# print("relevant chunks ",relevant_chunks)
+
+SYSTEM_PROMPT  = f"""
+You are a helpful AI Assistant who respond base of the avaliable context
+
+
+context:
+{relevant_chunks}
+
+"""
+
+result= client.chat.completions.create(
+    model="gpt-4",
+    messages=[
+        { "role":"system", "content":SYSTEM_PROMPT},
+        {"role":"user","content":"what is bert?"}
+    ]
+)
+
+print(result.choices[0].message.content)
